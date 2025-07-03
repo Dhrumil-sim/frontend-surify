8fb7fe7c6292d5e42fd2c45ee2a4c73f20e26b1a
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-07-03 16:07:47 +0530

    feat: added templating component

--- src/app/pages/user/user-home-page/user-home-page.component.ts
+++ src/app/pages/user/user-home-page/user-home-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
+import { SideBarComponent } from '../../../components/user/dashboard/side-bar/side-bar.component';
+import { UserHeaderComponent } from '../../../shared/components/user-header/user-header.component';
 
 @Component({
   selector: 'app-user-home-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBarComponent, UserHeaderComponent],
   templateUrl: './user-home-page.component.html',
   styleUrl: './user-home-page.component.css',
 })
